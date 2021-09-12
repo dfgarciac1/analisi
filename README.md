bc4f733b2f918f5a5d212dde27c3ba6e1ba64fd8
dfgarciac1 <53051443+dfgarciac1@users.noreply.github.com>
2021-09-12 05:03:28 +0000

    Initial commit
    Created from https://vercel.com/new

---
+++ README.md
@@ -0,0 +1,20 @@
+
+# NumericalAnalysis
+Repository for the project of Numerical analysis 
+
+# Team Members
+
+- David Felipe Garcia [![GitHub-Mark-Light-32px](https://user-images.githubusercontent.com/53051438/128283510-7d92c6a9-9c3e-4b22-b1ce-7786d951ef65.png)](https://github.com/dfgarciac1) dfgarciac1@eafit.edu.co
+- Ximena Guerrero Villa [![GitHub-Mark-Light-32px](https://user-images.githubusercontent.com/53051438/128283510-7d92c6a9-9c3e-4b22-b1ce-7786d951ef65.png)](https://github.com/xguerrerov1) xguerrerov@eafit.edu.co
+- Camilo Cañas Jaramillo [![GitHub-Mark-Light-32px](https://user-images.githubusercontent.com/53051438/128283510-7d92c6a9-9c3e-4b22-b1ce-7786d951ef65.png)](https://github.com/ccanasj) ccanasj@eafit.edu.co
+- Esteban Gonzalez Tamayo [![GitHub-Mark-Light-32px](https://user-images.githubusercontent.com/53051438/128283510-7d92c6a9-9c3e-4b22-b1ce-7786d951ef65.png)](https://github.com/egonzalezt) egonzalezt@eafit.edu.co
+
+# Project type
+
+For this project, we are going to develop the methods view in class of Numerical analysis. 
+
+# Init Proyect
+## Comands
+-For  run this repository you need node install
+-then run npm install
+- For RUN ALL npm dev
---
+++ pages/team/index.js
@@ -0,0 +1,86 @@
+import style from '../../styles/Card.module.css'
+
+export default function Team() {
+
+
+    return (
+        <div className="container">
+            <div className={style.container}>
+            <h1 className={style.title2}>Integrantes</h1>
+            <div class={style.boxesContainer}>
+                <div class={style.cardBox}>
+                    <div className={style.grid}>
+                        <div class={style.card}>
+                            <div class={style.front}>
+                                <h3>Camilo Cañas Jaramillo</h3>
+                                <p>Hover to flip</p>
+                                <strong>&#x21bb;</strong>
+                            </div>
+                            <div class={style.back}>
+                                <h3>Description</h3>
+                                <p>Github ,Grupo Miercoles</p>
+                                <a class={style.button} href="https://github.com/egonzalezt/NumericalAnalysis ">Github</a>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+                <div class={style.cardBox}>
+                    <div className={style.grid}>
+                        <div class={style.card}>
+                            <div class={style.front}>
+                                <h3>David Felipe García Contreras</h3>
+                                <p>Hover to flip</p>
+                                <strong>&#x21bb;</strong>
+                            </div>
+                            <div class={style.back}>
+                                <h3>Description</h3>
+                                <p>Github, Grupo Viernes</p>
+                                <a class={style.button} href="https://github.com/egonzalezt/NumericalAnalysis">Github</a>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+                <div class={style.cardBox}>
+                    <div className={style.grid}>
+                        <div class={style.card}>
+                            <div class={style.front}>
+                                <h3>Esteban Gonzalez Tamayo</h3>
+                                <p>Hover to flip</p>
+                                <strong>&#x21bb;</strong>
+                            </div>
+                            <div class={style.back}>
+                                <h3>Description</h3>
+                                <p>Github, Grupo Miercoles </p>
+                                <a class={style.button} href="https://github.com/egonzalezt/NumericalAnalysis">Github</a>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+                <div class={style.cardBox}>
+                    <div className={style.grid}>
+                        <div class={style.card}>
+                            <div class={style.front}>
+                                <h3>Ximena Guerrero</h3>
+                                <p>Hover to flip</p>
+                                <strong>&#x21bb;</strong>
+                            </div>
+                            <div class={style.back}>
+                                <h3>Description</h3>
+                                <p>Github, Grupo Miercoles </p>
+                                <a class={style.button} href="https://github.com/egonzalezt/NumericalAnalysis">Github</a>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+                        </div>
+                        </div>
+                        </div>
+
+
+
+                        )
+            
+    }
+
+    
+
